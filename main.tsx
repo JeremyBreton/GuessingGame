614886631b92e55382ff88f730bf0d3301dc4081
Jérémy <breton.jeremy@live.fr>
2024-01-29 15:16:21 +0100

    deplace main.tsx at root

--- main.tsx
+++ main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './components/App/App';
+import App from './src/components/App/App';
 
 import './styles/index.scss';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './src/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
